d62d9cf469220ace9d75521f9976856cee104320
ashwani161 <ashwanin65@gmail.com>
2021-03-10 08:10:44 +0530

    Added:Welcome to Employee

---
+++ src/com/employee/EmpWage.java
@@ -0,0 +1,10 @@
+package com.employee;
+
+public class EmpWage {
+	public static void main(String[] args) {
+		System.out.println("Welcome to Employee");
+		System.out.println("Wage Computation");
+		System.out.println("Program On Master Branch");
+	}
+
+}
