261a3a50f8e43df76e7d84b2e2c8473ba041de41
ashwani161 <ashwanin65@gmail.com>
2021-03-11 10:43:40 +0530

    Added:Calculating the employeeWage

--- src/com/employee/EmpWage.java
+++ src/com/employee/EmpWage.java
@@ -3,6 +3,9 @@ package com.employee;
 public class EmpWage {
 	public static void main(String[] args) {
 		int IS_FULL_TIME = 1;
+		int EMP_RATE_PER_HOUR = 20;
+		int empHrs = 0;
+		int empWage = 0;
 		System.out.println("Welcome to Employee");
 		System.out.println("Wage Computation");
 		System.out.println("Program On Master Branch");
@@ -11,5 +14,12 @@ public class EmpWage {
 			System.out.println("Employee is Present");
 		else
 			System.out.println("Employee is Absent");
+		if (empCheck == IS_FULL_TIME)
+			empHrs = 8;
+		else
+			empHrs = 0;
+		empWage = empHrs * EMP_RATE_PER_HOUR;
+		System.out.println("Emp Wage: " + empWage);
+		
 	}
 }
