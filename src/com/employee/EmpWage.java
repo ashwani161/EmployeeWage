77b2e48aaf8124b41add2bb9ea942e4c903bd546
ashwani161 <ashwanin65@gmail.com>
2021-03-11 10:29:03 +0530

    Added:Employee is Present or Absent

--- src/com/employee/EmpWage.java
+++ src/com/employee/EmpWage.java
@@ -2,9 +2,14 @@ package com.employee;
 
 public class EmpWage {
 	public static void main(String[] args) {
+		int IS_FULL_TIME = 1;
 		System.out.println("Welcome to Employee");
 		System.out.println("Wage Computation");
 		System.out.println("Program On Master Branch");
+		double empCheck = Math.floor(Math.random() * 10) % 2;
+		if (empCheck == IS_FULL_TIME)
+			System.out.println("Employee is Present");
+		else
+			System.out.println("Employee is Absent");
 	}
-
 }
