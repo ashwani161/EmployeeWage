1a0f0ff0e6b68b79dc751a043154fb1f18b8e29c
ashwani161 <ashwanin65@gmail.com>
2021-03-11 12:05:02 +0530

    Added:Calculating the monthly wages of a employee

--- src/com/employee/EmpWage.java
+++ src/com/employee/EmpWage.java
@@ -4,30 +4,34 @@ public class EmpWage {
 	public static final int IS_PART_TIME = 1;
 	public static final int IS_FULL_TIME = 2;
 	public static final int EMP_RATE_PER_HOUR = 20;
+	public static final int NUM_OF_WORKING_DAYS = 20;
 
 	public static void main(String[] args) {
-		int empHrs = 0;
+		int empHrs = 0, totalEmpWage = 0;
 		int empWage = 0;
 		System.out.println("Welcome to Employee");
 		System.out.println("Wage Computation");
 		System.out.println("Program On Master Branch");
 		int empCheck = (int) Math.floor(Math.random() * 10) % 3;
-		if(empCheck == IS_FULL_TIME)
+		if (empCheck == IS_FULL_TIME)
 			System.out.println("Employee is Present");
 		else
 			System.out.println("Employee is Absent");
-		switch (empCheck) {
-		  case IS_PART_TIME:
-			empHrs = 8;
-			break;
-		  case IS_FULL_TIME:
-			empHrs = 4;
-			break;
-		default:
-			empHrs = 0;
+		for (int day = 0; day < NUM_OF_WORKING_DAYS; day++) {
+			switch (empCheck) {
+			case IS_PART_TIME:
+				empHrs = 8;
+				break;
+			case IS_FULL_TIME:
+				empHrs = 4;
+				break;
+			default:
+				empHrs = 0;
+			}
+			empWage = empHrs * EMP_RATE_PER_HOUR;
+			totalEmpWage += empWage;
+			System.out.println("Emp Wage: " + empWage);
 		}
-		empWage = empHrs * EMP_RATE_PER_HOUR;
-		System.out.println("Emp Wage: " + empWage);
-		
+		System.out.println("Total Emp Wage: " + totalEmpWage);
 	}
 }
